9b7598948ed5be431272506cfd349435e112c325
Minh Nguyen <minhng@haposoft.com>
2025-01-21 11:33:12 +0700

    managing states between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+import NoProject from "./components/NoProject";
 import Project from "./components/Project";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <Project />
-    </main>
-  );
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <Project />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProject onAddProject={handleAddProject} />
+  }
+
+    return (
+      <main className="h-screen my-8 flex gap-8">
+        <Sidebar onAddProject={handleAddProject} />
+        {content}
+      </main>
+    );
 }
 
 export default App;
