3df0c8add7d7d464e0aab96e13c9fe8f5cc092c6
Minh Nguyen <minhng@haposoft.com>
2025-01-21 11:04:20 +0700

    sidebar + add project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Project from "./components/Project";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      <Project />
+    </main>
   );
 }
 
